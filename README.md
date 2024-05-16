38182624036af164f8fe40d50be83676ed782e5a
Ale <alelaalien@gmail.com>
2024-05-16 20:10:20 +0100

    tomas test

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # csharpTechnicalTests
-random technical tests found
+````````
+`1`
+`````````
+
+Tomas: 
+### Input Specifications
+
+The input will begin with an integer \(1 \leq N \leq 20\), indicating the number of test cases that will follow. The next \(N\) lines will each describe a test case. Each test case will start with an integer \(1 \leq K \leq 10\), indicating the number of power strips in the test case. On the same line, there will be \(K\) integers separated by spaces, \(O1, O2, \ldots, Ok\) where \(2 \leq Oi \leq 10\), indicating the number of outlets each power strip has.
+
+### Output Specifications
+
+The system must provide one line for each test case, with the maximum number of appliances that can be connected.
+
+### Input Example
+    3
+    3 2 3 4 
+    10 4 4 4 4 4 4 4 4 4 4 
+    4 10 10 10 10
+
+### Output Example
+
+ 7 
+ 31 
+ 37
+
+````````````````````````````````````````
---
+++ tomas/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Tomas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            initOperation();
+        }
+
+        static void initOperation()
+        { 
+            var numCases = NumCases();
+            
+            var tomas = new string[numCases];
+            
+            for (int i = 0; i < numCases; i++)
+            {
+                tomas[i] = Console.ReadLine();
+                
+            }
+
+            Slots(tomas);
+              
+        }
+
+     static int NumCases()
+        {
+            var isValid = false;
+            var numCases = 0;
+            while(!isValid || numCases <= 0)
+            {
+                System.Console.WriteLine("Please, enter a number greater than cero");
+                isValid = ValidateNumber(Console.ReadLine() ?? string.Empty, out numCases);
+                if(!isValid) System.Console.WriteLine("Not valid number!");
+            } 
+            return numCases;
+        }    
+        static bool ValidateNumber(string numberToValidate , out int number)
+        {
+            return   int.TryParse(numberToValidate, out number) && number > 0; 
+             
+        }
+
+        static void Slots(string[] tomas)
+        {
+            foreach (var toma in tomas)
+            {
+                var items = toma.Split(" ");
+                var count = 0;
+                var validated = ValidateNumber(items[0], out count);
+                if(!validated || items.Length - 1 != count) ShowGenericError();
+
+                var total = items.Aggregate(2, (acc, n) => acc + (int.Parse(n) - 1 ));
+                var result = total - int.Parse(items[0]) ; 
+                System.Console.WriteLine(result  );
+
+            }
+        }
+
+        static void ShowGenericError(){
+
+             System.Console.WriteLine("Something went wrong. Please try again");
+             Environment.Exit(0);
+         
+        }
+    }
+}
